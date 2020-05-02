621a7782f8c486516ef0a379c8f1048849ec496b
Daniel Aguilera <daguilera3220@gmail.com>
2020-05-02 11:41:37 -0600

    clean project

--- routes/web.php
+++ routes/web.php
@@ -16,19 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/sueldo_semanal', function () {
-    return view('sueldo_semanal');
-});
-Route::post('/sueldo_semanal', ['as' => 'Practica11.sueldo_semanal', "uses" => "Practica11Controller@calcularSueldo"]);
-Route::get('/febrero14', function () {
-    return view('febrero14');
-});
-Route::post('/febrero14', ['as' => 'Practica11.febrero14', "uses" => "Practica11Controller@calcularRegalo"]);
-Route::get('/descuento', function () {
-    return view('descuento');
-});
-Route::post('/descuento', ['as' => 'Practica11.descuento', "uses" => "Practica11Controller@calcularDescuento"]);
-Route::get('/becas', function () {
-    return view('becas');
-});
-Route::post('/becas', ['as' => 'Practica11.becas', "uses" => "Practica11Controller@calcularBeca"]);
