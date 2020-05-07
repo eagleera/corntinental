a2d1c282793b8148b46374a05f59b7fdcc143ff1
Daniel Aguilera <daguilera3220@gmail.com>
2020-05-06 18:41:54 -0600

    se arregla el restablecer contraseña fase 1

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,46 +2,27 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <div class="h-full card rounded-md p-6 has-background-light">
+        @if (session('status'))
+            <div class="notification is-success">
+                {{ session('status') }}
             </div>
-        </div>
+        @endif
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+            <div class="field">
+                <label class="label">Correo</label>
+                <div class="control">
+                    <input class="input @error('email') is-invalid @enderror" type="email" placeholder="danielaguilera@gg.com" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                </div>
+                @error('email')
+                    <p class="help is-danger">{{ $message }}</p>
+                @enderror
+            </div>
+            <button class="button is-primary is-fullwidth h-full text-xl font-bold" type="submit">
+                Enviar al correo instrucciones de recuperación de contraseña
+            </button>
+        </form>
     </div>
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,24 +19,42 @@
                 </div>
             </div>
             <div class="column is-6 h-full p-16">
-                <div class="border-4 border-gray-600 h-full rounded-md p-8">
-                    <div>
-                        <p class="font-bold text-4xl">Hola!</p>
-                        <p class="text-3xl">Para empezar a llevar la cuenta de tus juegos y puntuaciones inicia sesión o crea una cuenta!</p>
-                    </div>
-                    <div class="columns mt-16 is-multiline">
-                        <div class="column is-12">
-                            <a class="button is-primary is-fullwidth h-full text-xl font-bold" href="{{route('login')}}">
-                                Iniciar sesión
-                            </a>
+                @guest
+                    <div class="border-4 border-gray-600 h-full rounded-md p-8">
+                        <div>
+                            <p class="font-bold text-4xl">Hola!</p>
+                            <p class="text-3xl">Para empezar a llevar la cuenta de tus juegos y puntuaciones inicia sesión o crea una cuenta!</p>
                         </div>
-                        <div class="column is-12">
-                            <a class="button is-danger is-fullwidth h-full text-xl font-bold" href="{{route('register')}}">
-                                Crear una cuenta
-                            </a>
+                        <div class="columns mt-16 is-multiline">
+                            <div class="column is-12">
+                                <a class="button is-primary is-fullwidth h-full text-xl font-bold" href="{{route('login')}}">
+                                    Iniciar sesión
+                                </a>
+                            </div>
+                            <div class="column is-12">
+                                <a class="button is-danger is-fullwidth h-full text-xl font-bold" href="{{route('register')}}">
+                                    Crear una cuenta
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
+                @else
+                    <div class="border-4 border-gray-600 h-full rounded-md p-8 relative">
+                        <p class="text-2xl font-bold">Tu historial de partidas</p>
+                        @auth
+                            <a
+                                class="has-text-link absolute bottom-0 right-0 p-2"
+                                href="{{ url("logout") }}"
+                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                            >
+                                Cerrar sesión
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        @endauth
+                    </div>
+                @endguest
             </div>
         </div>
     </div>
