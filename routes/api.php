0d9f51757d38dc416fec888b569620f96544f93e
Daniel Aguilera <daguilera3220@gmail.com>
2020-05-26 16:23:31 -0600

    agrupar rutas

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::group(function() {
+Route::group([],function() {
     Route::middleware('player')->get('/juego/{id}', 'JuegoController@index');
     Route::middleware('player')->get('/winners/{id}', 'JuegoController@getWinners');
     Route::middleware('auth:api')->get('/record', 'JuegoController@record');
